debf08ad61db169d71b14a90864b98ab60066ef2
guojigang <guojigang@putao.com>
2021-10-15 23:21:50 +0800

    add route

--- resources/views/layout/default.blade.php
+++ resources/views/layout/default.blade.php
@@ -9,8 +9,8 @@
     <div class="container">
         <a class="navbar-brand" href="/">微博APP</a>
         <ul class="navbar-nav justify-content-end">
-            <li class="nav-item"><a class="nav-link" href="/help">帮助</a> </li>
-            <li class="nav-item"><a class="nav-link" href="/about">登录</a> </li>
+            <li class="nav-item"><a class="nav-link" href="{{route('help')}}">帮助</a> </li>
+            <li class="nav-item"><a class="nav-link" href="{{route('about')}}">关于</a> </li>
         </ul>
     </div>
 </nav>
--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,5 @@
 //    return view('welcome');
 //});
 Route::get('/', 'StaticPagesController@home');
-Route::get('/help', 'StaticPagesController@help');
-Route::get('/about', 'StaticPagesController@about');
+Route::get('/help', 'StaticPagesController@help')->name('help');
+Route::get('/about', 'StaticPagesController@about')->name('about');
