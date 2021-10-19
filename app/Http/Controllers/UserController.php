1bd4178db1d08965f7d60250972bbc5712b894fd
guojigang <guojigang@putao.com>
2021-10-19 11:14:53 +0800

    complete user register

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,8 +13,33 @@ class UserController extends Controller
         return view('users.create');
     }
 
+    /**
+     * @param User $user
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\Foundation\Application|\Illuminate\View\View
+     */
     public function show(User $user)
     {
         return view('users.show', compact('user'));
+    }
+
+    /**
+     * @param Request $request
+     * @throws \Illuminate\Validation\ValidationException
+     */
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'email'=>'required|email|unique:users|max:255',
+            'password'=>'required|confirmed|min:6'
+        ]);
+        $user = User::create([
+            'name'=>$request->name,
+            'email'=>$request->email,
+            'password'=>bcrypt($request->password),
+        ]);
+        session()->flash('success', '欢迎，您注册');
+        return redirect()->route('users.show',[$user]);
     }
 }
--- resources/views/layout/default.blade.php
+++ resources/views/layout/default.blade.php
@@ -14,6 +14,7 @@
         </ul>
     </div>
 </nav>
+@include('shared._message')
 @yield('content')
 </body>
 </html>
