6b62b3f0bfda078b3c7fba62e6a11deabe0a9b06
guojigang <guojigang@putao.com>
2021-10-19 10:34:27 +0800

    用户显示页面

--- config/database.php
+++ config/database.php
@@ -54,6 +54,7 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
+//            'unix_socket' => '/tmp/mysql.sock'
         ],
 
         'pgsql' => [
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,4 @@ Route::get('/', 'StaticPagesController@home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
 Route::get('/register', 'UserController@create')->name('register');
+Route::resource('users', 'UserController');
