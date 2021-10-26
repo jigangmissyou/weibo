183b7f555d2520b9ff2d711ba92542526947a47e
guojigang <guojigang@putao.com>
2021-10-26 16:11:15 +0800

    删除微博

--- resources/views/status/_status.blade.php
+++ resources/views/status/_status.blade.php
@@ -2,7 +2,14 @@
     <a href="{{route('users.show', $user->id)}}"><img src="{{$user->gravatar()}}" class="mr-3 gravatar"></a>
     <div class="media-body">
         <h5 class="mt-0 mb-1">
-            {{$user->name}}<small>{{$status->content}}</small> / {{$status->created_at->diffForHumans()}}
+            {{$user->name}}<small>/ {{$status->created_at->diffForHumans()}}</small> {{$status->content}}
         </h5>
     </div>
+    @can('destroy', $status)
+        <form action="{{route('statuses.destroy',$status->id)}}" method="POST" onsubmit="if (!confirm('确定要删除吗？')) return false;">
+            {{csrf_field()}}
+            {{method_field('DELETE')}}
+            <button type="submit" class="btn btn-sm btn-danger">删除</button>
+        </form>
+    @endcan
 </li>
